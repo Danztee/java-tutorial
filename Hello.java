7d06a680e41090a513b8b24e3e3226ed61c47938
Olowoniyi Daniel <olowoniyidaniel@gmail.com>
2023-11-27 10:34:17 +0100

    learning variables and data types

---
+++ Hello.java
@@ -0,0 +1,21 @@
+class Hello {
+
+  public static void main(String a[]) {
+    int num1 = 3;
+    int num2 = 5;
+    int result = num1 + num2;
+
+    byte num = 8;
+    System.out.println(result);
+
+    byte by = 127;
+    short sh = 558;
+    long l = 5854l;
+
+
+    float f  = 5.8f;
+    char c  = 'k';
+    boolean b = true;
+
+  }
+}
