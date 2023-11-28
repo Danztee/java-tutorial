e10184181f4f222b9cbb8b5e930452df62d0d719
Olowoniyi Daniel <olowoniyidaniel@gmail.com>
2023-11-28 18:19:16 +0100

    Day 2 learning java

--- Hello.java
+++ Hello.java
@@ -1,21 +1,57 @@
 class Hello {
 
-  public static void main(String a[]) {
-    int num1 = 3;
-    int num2 = 5;
-    int result = num1 + num2;
+  public static void main(String args[]) {
+    // int num1 = 3;
+    // int num2 = 5;
+    // int result = num1 + num2;
 
-    byte num = 8;
+    // byte num = 8;
+    // System.out.println(result);
+
+    // byte by = 127;
+    // short sh = 558;
+    // long l = 5854l;
+
+    // float f = 5.8f;
+    // char c = 'k';
+    // boolean b = true;
+
+    // literals
+    // char num1 = 'b';
+    // System.out.println(num1);
+
+    // type conversion
+
+    // byte b = 127;
+    // int a = b;
+
+    // int a =257;
+    // byte k = (byte) a;
+
+    // float f = 5.6f;
+
+    // int t = (int) f;
+
+    // type emorsion
+    // byte a = 10;
+    // byte b = 20;
+
+    // int result = a * b;
+
+    // int num = 5;
+
+    // int result = num++;
+
+    // OPERATORS
+    // System.out.println(num);
+    // System.out.println(result);
+
+    int x = 6;
+    int y = 7;
+    int a = 5;
+    int b = 9;
+    boolean result = x > y && a < b;
     System.out.println(result);
-
-    byte by = 127;
-    short sh = 558;
-    long l = 5854l;
-
-
-    float f  = 5.8f;
-    char c  = 'k';
-    boolean b = true;
 
   }
 }
