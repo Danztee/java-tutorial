eec0ed59ce4be4ed429afab35708cd05272ba86c
Olowoniyi Daniel <olowoniyidaniel@gmail.com>
2023-12-03 16:32:19 +0100

    day 6

--- Hello.java
+++ Hello.java
@@ -136,22 +136,22 @@ class Calculator {
 
   int a;
 
-  public int add() {
-    System.out.println("in add");
-    return 0;
+  public int add(int n1, int n2) {
+    int r = n1 + n2;
+    return r;
   }
 }
 
-public class Demo {
+public class Hello {
 
-  public static void main(String[] args) {
+  public static void main(String a[]) {
     int num1 = 4;
     int num2 = 5;
 
     Calculator calc = new Calculator();
-    calc.add();
+    int result = calc.add(num1, num2);
     // int result = num1 + num2;
 
-    // System.out.println(result);
+    System.out.println(result);
   }
 }
