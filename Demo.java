c870d2d7d4de1e0a0a361ea8a83e9ab2afd9ed51
Olowoniyi Daniel <olowoniyidaniel@gmail.com>
2023-12-07 20:18:44 +0100

    day 11

--- Demo.java
+++ Demo.java
@@ -41,22 +41,68 @@ public class Demo {
     // }
 
     // JAGGED ARRAYS
-    int nums[][] = new int[3][];
-    nums[0] = new int[3];
-    nums[1] = new int[4];
-    nums[2] = new int[5];
+    // int nums[][] = new int[3][];
+    // nums[0] = new int[3];
+    // nums[1] = new int[4];
+    // nums[2] = new int[5];
 
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = 0; j < nums.length; j++) {
-        nums[i][j] = (int) (Math.random() * 100);
-      }
-    }
+    // for (int i = 0; i < nums.length; i++) {
+    //   for (int j = 0; j < nums.length; j++) {
+    //     nums[i][j] = (int) (Math.random() * 100);
+    //   }
+    // }
 
-    for (int n[] : nums) {
-      for (int m : n) {
-        System.out.print(m + " ");
-      }
-      System.out.println();
+    // for (int n[] : nums) {
+    //   for (int m : n) {
+    //     System.out.print(m + " ");
+    //   }
+    //   System.out.println();
+    // }
+
+    int nums[] = new int[6];
+    nums[0] = 1;
+    nums[1] = 4;
+    nums[2] = 7;
+    nums[3] = 12;
+
+    // for (int n : nums) {
+    //   System.out.println(n);
+    // }
+    Student s1 = new Student();
+    s1.rollNo = 1;
+    s1.name = "Daniel";
+    s1.marks = 88;
+
+    Student s2 = new Student();
+    s2.rollNo = 2;
+    s2.name = "Bolu";
+    s2.marks = 93;
+
+    Student s3 = new Student();
+    s3.rollNo = 3;
+    s3.name = "Kehinde";
+    s3.marks = 68;
+
+    // System.err.println(s1.name);
+
+    Student students[] = new Student[3];
+
+    students[0] = s1;
+    students[1] = s2;
+    students[2] = s3;
+
+    for (Student stu : students) {
+      System.out.println(stu.name + " " + stu.marks);
     }
+    // for (int i = 0; i < students.length; i++) {
+    //   System.err.println(students[i].name + " " + students[i].marks);
+    // }
   }
+}
+
+class Student {
+
+  int rollNo;
+  String name;
+  int marks;
 }
