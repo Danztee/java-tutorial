3628e612ddeec9a33fcc235a692f9c6c3266d4f4
Olowoniyi Daniel <olowoniyidaniel@gmail.com>
2023-12-25 20:39:08 +0100

    day 29

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,14 +20,33 @@ public class Demo {
     //   System.out.println(n);
     // }
 
-    Map<String, Integer> students = new HashMap<>();
-    students.put("Daniel", 25);
-    students.put("King", 15);
-    students.put("Bolu", 30);
-    students.put("kira", 70);
+    // Map<String, Integer> students = new HashMap<>();
+    // students.put("Daniel", 25);
+    // students.put("King", 15);
+    // students.put("Bolu", 30);
+    // students.put("kira", 70);
 
-    for (String key : students.keySet()) {
-      System.out.println(key + " " + students.get(key));
-    }
+    // for (String key : students.keySet()) {
+    //   System.out.println(key + " " + students.get(key));
+    // }
+
+    Comparator<Integer> com = new Comparator<>() {
+      public int compare(Integer i, Integer j) {
+        if (i % 10 > j % 10) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+    };
+
+    List<Integer> nums = new ArrayList<>();
+    nums.add(23);
+    nums.add(12);
+    nums.add(76);
+
+    Collections.sort(nums, com);
+
+    System.out.println(nums);
   }
 }
